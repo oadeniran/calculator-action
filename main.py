1412015b989a3c1188b8693443cf1af010c4d130
oadeniran <60529828+oadeniran@users.noreply.github.com>
2023-06-16 12:23:44 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+def add_function(x, y):
+    return x + y
+
+def subtract_function(x, y):
+    return x - y
+
+def multiply_function(x, y):
+    return x * y
+
+def divide_function(x, y):
+    return x / y
+
+print('Welcome calculator')
+print(''' SELECT OPTION (number-only)
+        1) Addition
+        2) Subtraction
+        3) Multiplication
+        4) Division
+        ''')
+choice = int(input('selection -> '))
+num1 = float(input('Number1 - '))
+num2 = float(input('Number2 - '))
+if choice == 1:
+    print(add_function(num1, num2))
+elif choice == 2:
+    print(subtract_function(num1, num2))
+elif choice == 3:
+    print(multiply_function(num1, num2))
+else:
+    print(divide_function(num1, num2))
+    
